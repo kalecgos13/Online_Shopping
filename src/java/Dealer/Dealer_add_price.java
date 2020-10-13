8d3e8bcfd202135c3bbf64acbd7c927379100935
kalecgos13 <kalecgos13@gmail.com>
2020-10-13 15:50:18 +0200

    added prepared statements to Dealer package

--- src/java/Dealer/Dealer_add_price.java
+++ src/java/Dealer/Dealer_add_price.java
@@ -28,13 +28,10 @@ public class Dealer_add_price extends HttpServlet
         try
         {
             Database_connection obj_connection = new Database_connection();
-            Connection cnn = obj_connection.cnn;
-            Statement st = cnn.createStatement();
-            Statement st1 = cnn.createStatement();
             DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
             java.util.Date date = new java.util.Date();
             boolean b = false;
-            ResultSet rs = st.executeQuery("select * from tbl_dealer where l_id = "+usersession.getAttribute("dealer")+" and p_id = "+req.getParameter("pid"));
+            ResultSet rs = obj_connection.doPreparedQuery("select * from tbl_dealer where l_id = ? and p_id = ?", new int[]{0,0}, new Object[]{usersession.getAttribute("dealer"),Integer.parseInt(req.getParameter("pid"))});
             while(rs.next())
             {
                 b = true;
@@ -42,11 +39,10 @@ public class Dealer_add_price extends HttpServlet
             
             if(b)
             {
-                st.execute("delete from tbl_dealer where l_id ="+ usersession.getAttribute("dealer") +" and p_id ="+req.getParameter("pid"));
+                obj_connection.doPreparedUpdate("delete from tbl_dealer where l_id = ? and p_id = ?", new int[]{0,0}, new Object[]{usersession.getAttribute("dealer"),Integer.parseInt(req.getParameter("pid"))});
             }
             
-            
-            st1.execute("insert into tbl_dealer values(null,"+ usersession.getAttribute("dealer") +","+ req.getParameter("pid") +","+req.getParameter("d_price") +",'"+dateFormat.format(date)+"')");
+            obj_connection.doPreparedUpdate("insert into tbl_dealer values(null,?,?,?,?)", new int[]{0,0,0,1}, new Object[]{usersession.getAttribute("dealer"), Integer.parseInt(req.getParameter("pid")),Integer.parseInt(req.getParameter("d_price")),dateFormat.format(date)});
       
             
             String referer = req.getHeader("Referer");
