e9696ffc9691ed26cb6eb4e131cde2c6943b8d72
kalecgos13 <kalecgos13@gmail.com>
2020-10-13 09:47:31 +0200

    added main method in Database_connection.java for PreparedStatements

--- src/java/database/Database_connection.java
+++ src/java/database/Database_connection.java
@@ -12,4 +12,21 @@ public class Database_connection {
         cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/online_shopping?noAccessToProcedureBodies=true", "onlineshopping", "bot123456789");
         //   cnn = DriverManager.getConnection("jdbc:mysql://mariadb15859-saikiran.cloudhosting.rsaweb.co.za/online_db", "root", "BDGkro86104");      
     }
+    
+    public ResultSet doPrepared(String query, int[] types, String[] arguments) throws SQLException {
+		PreparedStatement st = cnn.prepareStatement(query);
+		for(int i = 0;i<types.length;i++) {
+			/*
+			 * 0 = int
+			 * 1 = string
+			 */
+			if(types[i] == 0) {
+				st.setInt(1+i, Integer.parseInt(arguments[i]));
+			}
+			else if (types[i] == 1) {
+				st.setString(1+i, arguments[i]);
+			}
+		}
+		return st.executeQuery();
+	}
 }
