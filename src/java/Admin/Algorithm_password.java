3626df6be0e08429aa24781bcd52853e86aae0e4
kalecgos13 <kalecgos13@gmail.com>
2020-10-15 11:56:27 +0200

    Removed unused functions and imports

--- src/java/Admin/Algorithm_password.java
+++ src/java/Admin/Algorithm_password.java
@@ -1,11 +1,8 @@
 package Admin;
 
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -40,26 +37,5 @@ public class Algorithm_password {
         catch(NoSuchAlgorithmException | DecoderException | InvalidKeySpecException e) {
             throw new RuntimeException(e);
         }
-    }
-    
-    
-    private final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-    public String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-    public byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                                 + Character.digit(s.charAt(i+1), 16));
-        }
-        return data;
     }
 }
